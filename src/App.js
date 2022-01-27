b566b5b5df618abca1c97c91c941175866d9066c
mattgefen <matt.gefen@gmail.com>
2022-01-27 13:38:47 -0700

    basic landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// import { useState, useEffect } from 'react';
+
+// import getBooks from './services/books';
+
+import Landing from './components/Landing/Landing';
 
 function App() {
+
   return (
     <div className="App">
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from './Landing.module.css'
+
+
+const Landing = (props) => {
+
+  return (
+    <div className={styles.landing}>
+      <div className ={styles.heading}>What Book Do You Seek?</div>
+    </div>
+  )
+}
+
+export default Landing
